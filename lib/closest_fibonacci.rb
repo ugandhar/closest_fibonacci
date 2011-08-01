0e6747df48915df32ca2f511f79147fe8e5f40b6
ugandhar <ugandharc18@gmail.com>
2011-07-31 22:36:26 -0400

    finished_code_with_test_cases

---
+++ lib/closest_fibonacci.rb
@@ -0,0 +1,18 @@
+class Fixnum
+  def closest_fibonacci
+    (return nil) if self < 0
+    fn = 0
+    fn1 , fn2 = 0 , 1
+    if self == 0
+      return 0
+    elsif self == 1
+      return 1
+    else
+      while self >= fn
+        fn = fn1 + fn2
+        fn1, fn2   = fn2, fn
+      end
+    end
+    fn1
+  end
+end
